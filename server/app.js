be279340aa68b9f455fda27f96dea423531f6e6e
sunchichi0505 <sunchichi0505@gmail.com>
2021-10-26 17:24:09 +0900

    webapp Update server>app.js(21.10.26)

--- server/app.js
+++ server/app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+
+app.use(session({
+    secret: 'secret code',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true,
+        maxAge: 1000 * 60 * 60 //cookie有効期限
+    }
+}));
